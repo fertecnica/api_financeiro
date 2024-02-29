238e8b4ef92f224d3dba01b89d0a3963c12d13ea
Hassanfertec <hassan.primo@fertecnica.com>
2024-02-29 10:49:59 -0300

    Adiciona a classe WebNavigator que utiliza o webdriver do Edge para navegar na web. A classe aceita o caminho do driver e o caminho do plugin (.crx)

--- web_navigator.py
+++ web_navigator.py
@@ -4,3 +4,13 @@ from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+class WebNavigator:
+    def __init__(self, driver_path, extension_path):
+        edge_options = Options()
+        edge_service = Service(driver_path)
+
+        # Adiciona a extensao .crx
+        edge_options.add_extension(extension_path)
+
+        self.driver = webdriver.Edge(service=edge_service, options=edge_options)
