5541376d9252b1e4b3417e389f0ba98c6e09e0f2
Hassanfertec <hassan.primo@fertecnica.com>
2024-02-29 10:55:47 -0300

    Adicionei alguns metodos a classe WebNavigator que permite a navegaçao e a interaçao com a pagina.

--- web_navigator.py
+++ web_navigator.py
@@ -14,3 +14,21 @@ class WebNavigator:
         edge_options.add_extension(extension_path)
 
         self.driver = webdriver.Edge(service=edge_service, options=edge_options)
+
+    # Navega para a página da web especificada
+    def navigate_to_page(self, url):
+        self.driver.get(url)
+
+    # Retorna o driver do navegador atualmente em uso
+    def get_driver(self):
+        return self.driver
+
+    # Aguarda até que o elemento esteja visível e então clica nele
+    def click_element(self, locator, locator_type=By.XPATH, wait_time=10):
+        element = WebDriverWait(self.driver, wait_time).until(EC.visibility_of_element_located((locator_type, locator)))
+        element.click()
+
+    # Aguarda até que o campo de entrada esteja visível e então preenche com o texto fornecido
+    def fill_input_field(self, locator, input_text, locator_type=By.XPATH, wait_time=10):
+        input_field = WebDriverWait(self.driver, wait_time).until(EC.visibility_of_element_located((locator_type, locator)))
+        input_field.send_keys(input_text)
