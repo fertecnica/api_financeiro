66dca5791b82dc51e577d54ff046c1c493e312d7
Hassanfertec <hassan.primo@fertecnica.com>
2024-02-29 13:18:09 -0300

    Adiciona a classe DataProcessor

--- data_process.py
+++ data_process.py
@@ -1,2 +1,7 @@
-from data_analysis_tools import DataAnalysisTools
+from data_tools import DataTools
 import pandas as pd
+
+class DataProcessor:
+    def __init__(self, download_path):
+        self.download_path = download_path
+        self.tools = DataTools(download_path)
