98bf4ffe808f6e50720e3e20d57a633b4349626d
Hassanfertec <hassan.primo@fertecnica.com>
2024-02-29 11:23:29 -0300

    Renomeei a classe 'BankScrapper' para 'FlashScrapper'

--- flash_scrapper.py
+++ flash_scrapper.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-class BankScrapper:
+class FlashScrapper:
     def __init__(self, web_navigator):
         self.web_navigator = web_navigator
         self.driver = self.web_navigator.get_driver()
