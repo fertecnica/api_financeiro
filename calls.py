df4a295a316cf917a292b16f79d2643789bdc889
Hassanfertec <hassan.primo@fertecnica.com>
2024-02-29 11:44:36 -0300

    Integra a classe FlashScrapper ao endpoint 'busca_despesas_flash'

--- calls.py
+++ calls.py
@@ -9,4 +9,19 @@ def index():
     # Inicialize o WebNavigator
     navigator = WebNavigator('C:\\Users\\HassanPrimo\\OneDrive - FERTECNICA FERRAMENTAS E EQUIPAMENTOS LTDA\\Aplicativos\\msedgedriver.exe', 'C:\\Users\\HassanPrimo\\Downloads\\recaptcha.crx')
 
+    # Acessa a pagina de login do relatorio
+    navigator.navigate_to_page('https://app.expenseon.com/admin/login?ReturnURL=/admin/financeiro/relatorio')
+
+    # Inicialize o BankScrapper
+    scrapper = FlashScrapper(navigator)
+
+    # Execute o processo de login
+    scrapper.login_process()
+
+    # Acessa a pagina de Relatorio
+    navigator.navigate_to_page('https://app.expenseon.com/admin/financeiro/relatorio')
+
+    # Execute o processo de scrapping
+    scrapper.scrapper_process()
+
     return 'OK'
